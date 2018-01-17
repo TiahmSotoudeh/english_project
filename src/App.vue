da526472d0c030797a16a4bf41c742a8696a94bf
Tiahm Sotoudeh <sotoudehtiahm@gmail.com>
2018-01-17 14:45:56 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: 'home',
+          title: 'introduction'
+        },
+        {
+          icon: 'format_quote',
+          title: 'quote 1'
+        },
+        {
+          icon: 'format_quote',
+          title: 'quote 2'
+        },
+        {
+          icon: 'format_quote',
+          title: 'quote 3'
+        },
+        {
+          icon: 'close',
+          title: 'conclusion'
+        },
+      ],
+      title: 'english eq project'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <!--introdction layout-->
+    <v-layout transition="slide-x-transition">
+      <v-flex xs12>
+        <h1>english essential question project</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
